20d97fe26152589aee7f003f2526f0a335959acb
Jefferson <jefferson.senac1@yahoo.com>
2024-04-15 19:36:13 -0300

    consertando bugs

--- index.js
+++ index.js
@@ -1,87 +1,110 @@
-let api = 'https://api.themoviedb.org/3/movie/'
-let data = [];
+const api = 'https://api.themoviedb.org/3/movie/';
+const apiKey = '1728816a402a07893f04b1af16b895cd';
+const language = 'pt-BR';
+const page = 1;
+const container = document.querySelector('.container');
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+const filmeInput = document.querySelector('#pesquisaFilme');
+
+let movies = [];
 
 
-async function loadFilmes(){
-    try{
-        const apiKey = '1728816a402a07893f04b1af16b895cd';
-        const language = 'pt-BR';
-        const page = 1;
 
-        const response = await fetch(`${api}/now_playing?api_key=${apiKey}&language=${language}&page=${page}`);
-            if(response.status === 200){
-                data = (await response.json()).results;
-                    console.log(data)
-                    filmes(data)
-            }else if(response.status === 404){
-                alert('erro')
-            }
-    }
-    catch{
-        console.log('error')
+
+async function fetchData(endpoint) {
+    try {
+        const response = await fetch(`${api}${endpoint}?api_key=${apiKey}&language=${language}&page=${page}`);
+        if (response.ok) {
+            const data = await response.json();
+            movies = data.results;
+            return data;
+        } else {
+            throw new Error('Erro na solicitação');
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
 
-loadFilmes()
-
-let filmeInput = document.querySelector('#pesquisaFilme')
-filmeInput.addEventListener('input', (e)=>{
-    let container = document.querySelector('.container')
-    let filmeInputValue = e.target.value.toLowerCase();
-    console.log(filmeInputValue)
-
-    if(data){
-       let filmeFiltrado = data.filter((filme)=> filme.title.toLowerCase().includes(filmeInputValue))
-       console.log(filmeFiltrado)
-        
-        // Limpa o conteúdo anterior do container
-    container.innerHTML = '';
-
-    // Renderiza os filmes filtrados
-    filmes(filmeFiltrado);
+async function loadFilmes() {
+    try {
+        const data = await fetchData('now_playing');
+        filmes(data.results);
+    } catch (error) {
+        console.error(error);
     }
+}
 
-    
-  
-})
- 
-async function filmes(data){
-    let container = document.querySelector('.container')
-        container.innerHTML = '';
-  
+async function loadPopularFilmes() {
+    try {
+        const data = await fetchData('popular');
+        filmesPopulares(data.results);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-        data.map((filme) => {
-            let filmediv = document.createElement('div');
-        filmediv.classList.add('filme'); // Define a classe diretamente aqui
-        let titulodiv = document.createElement('div');
-        titulodiv.classList.add('titulo'); // Define a classe diretamente aqui
-        let imagediv = document.createElement('img');
-        imagediv.classList.add('image'); // Define a classe diretamente aqui
-        let vote = document.createElement('div');
-        vote.classList.add('vote'); // Def
-        let estrela = document.createElement('img')
-        estrela.classList.add('estrela')    
-        estrela.src = './img/estrela.png'    
-        // Cria um elemento <a> para o link do trailer
-    let trailerLink = document.createElement('a');
+function filmesPopulares(data) {
+    swiperWrapper.innerHTML = '';
+    data.forEach((filme) => {
+        let slider = document.createElement('div')
+        slider.classList.add('swiper-slide')
+        slider.style.width = '300px'; // Defina uma largura fixa para os slides
+        const filmediv = createFilmDiv(filme);
+        swiperWrapper.appendChild(slider);
+        slider.appendChild(filmediv)
+    });
+    swiper.update(); // Atualize o Swiper após adicionar os slides
+}
+
+
+function filmes(data) {
+    container.innerHTML = '';
+    data.forEach((filme) => {
+        const filmediv = createFilmDiv(filme);
+        container.appendChild(filmediv);
+    });
+}
+
+function createFilmDiv(filme) {
+    const filmediv = document.createElement('div');
+    filmediv.classList.add('filme');
+
+    const titulodiv = document.createElement('div');
+    titulodiv.classList.add('titulo');
+    titulodiv.innerText = filme.title;
+
+    const imagediv = document.createElement('img');
+    imagediv.classList.add('image');
+    imagediv.src = `https://image.tmdb.org/t/p/original/${filme.poster_path}`;
+
+    const vote = document.createElement('div');
+    vote.classList.add('vote');
+    vote.innerHTML = `Score: ${filme.vote_average.toFixed(1)}/10 `;
+
+    const trailerLink = document.createElement('a');
     trailerLink.href = `https://www.youtube.com/results?search_query=${filme.title} Trailer`;
-    trailerLink.target = '_blank'; // Abre o link em uma nova aba
+    trailerLink.target = '_blank';
 
-    // Cria o botão dentro do elemento <a>
-    let btn = document.createElement('button');
+    const btn = document.createElement('button');
     btn.innerText = 'Trailer';
     btn.classList.add('btn');
 
-    // Adiciona o botão dentro do elemento <a>
-    vote.appendChild(estrela)
-    trailerLink.appendChild(btn);       
-    titulodiv.innerText = filme.title;
-    imagediv.src = `https://image.tmdb.org/t/p/original/${filme.poster_path}`;
-    vote.innerText = `${filme.vote_average}/10`;
+    trailerLink.appendChild(btn);
+    vote.appendChild(trailerLink);
 
-    // Adiciona os elementos ao div "filme"
-    filmediv.append(titulodiv, imagediv, vote, trailerLink);
+    filmediv.append(titulodiv, imagediv, vote);
 
-    container.append(filmediv);
-        })
+    return filmediv;
 }
+
+loadFilmes();
+loadPopularFilmes();
+
+filmeInput.addEventListener('input', (e) => {
+    const filmeInputValue = e.target.value.toLowerCase();
+    if (movies) {
+        const filmeFiltrado = movies.filter((filme) => filme.title.toLowerCase().includes(filmeInputValue));
+        filmes(filmeFiltrado);
+    }
+});
